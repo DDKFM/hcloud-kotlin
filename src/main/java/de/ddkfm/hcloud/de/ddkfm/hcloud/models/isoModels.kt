c083422de52f57577acff9c41e85946ad986c4dc
Michél Popp <michel_popp@web.de>
2018-02-18 09:33:41 +0100

    add iso part

---
+++ src/main/java/de/ddkfm/hcloud/de/ddkfm/hcloud/models/isoModels.kt
@@ -0,0 +1,8 @@
+package de.ddkfm.hcloud.de.ddkfm.hcloud.models
+
+data class isoModel(
+        var id: Int,
+        var name: String,
+        var description: String,
+        var type: String
+)
---
+++ src/main/java/de/ddkfm/hcloud/dev_iso.kt
@@ -0,0 +1,58 @@
+package de.ddkfm.hcloud.de.ddkfm.hcloud.models
+
+import com.mashape.unirest.http.Unirest
+import org.json.JSONObject
+
+class dev_iso {
+    var auth: String = ""
+    var endpoint: String = "https://api.hetzner.cloud/v1";
+
+    constructor(token: String) {
+        this.auth = "Bearer $token";
+    }
+
+    // retrieve all available ISOs
+    fun getIsos() : List<isoModel> {
+        var req = Unirest
+                .get("$endpoint/isos")
+                .header("Authorization", auth);
+        val jsonResp = req
+                .asJson()
+                .body
+                .`object`;
+        var isoRAW = jsonResp.getJSONArray("isos");
+        var returnList = mutableListOf<isoModel>();
+        isoRAW.forEach {
+            //Kotlin-Magic: "it" is automatically the current element in the JSONArray
+            val jsonISO : JSONObject = it as JSONObject;
+            var iso = isoModel(
+                    id = jsonISO.getInt("id"),
+                    name = jsonISO.getString("name"),
+                    description = jsonISO.getString("description"),
+                    type = jsonISO.getString("type")
+            );
+            returnList.add(iso);
+        }
+        return returnList;
+    }
+
+    fun getOneIso(id: String): isoModel{
+        var req = Unirest
+                .get("$endpoint/isos/"+id)
+                .header("Authorization", auth);
+        val jsonResp = req
+                .asJson()
+                .body
+                .`object`;
+        var jsonISO = jsonResp.getJSONObject("iso");
+
+        var iso = isoModel(
+                id = jsonISO.getInt("id"),
+                name = jsonISO.getString("name"),
+                description = jsonISO.getString("description"),
+                type = jsonISO.getString("type")
+        );
+
+        return iso;
+    }
+}
