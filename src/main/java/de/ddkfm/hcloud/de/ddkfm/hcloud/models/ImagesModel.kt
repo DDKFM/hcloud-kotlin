dd562b92c19963d9a1fdd2a0cea54d6e47ee3a4a
Michél Popp <michel_popp@web.de>
2018-02-20 17:54:22 +0100

    add Images data class

---
+++ src/main/java/de/ddkfm/hcloud/de/ddkfm/hcloud/models/ImagesModel.kt
@@ -0,0 +1,22 @@
+package de.ddkfm.hcloud.de.ddkfm.hcloud.models
+
+data class image(
+        var id: Int,
+        var type: String,
+        var status: String,
+        var name: String,
+        var description: String,
+        var ImageSize: Double,
+        var DiskSize: Double,
+        var created: String,
+        var createdFrom: CreateFromData,
+        var BoundTo: Int,
+        var OsFlavor: String,
+        var OsVersion: String,
+        var RapidDeploy: Boolean
+)
+
+data class CreateFromData(
+        var id: Int,
+        var name: String
+)
