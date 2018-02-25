a319eea0cd5c7798a7aad54654283b64bddd2d47
Michel <michel_popp@web.de>
2018-02-25 13:39:29 +0100

    delete some lines

--- src/main/java/de/ddkfm/hcloud/PricesApi.kt
+++ src/main/java/de/ddkfm/hcloud/PricesApi.kt
@@ -18,8 +18,7 @@ class PricesApi(token : String) : ApiBase(token = token) {
                 .asJson()
                 .body
                 .`object`;
-        val jsonPrice = jsonResp.getJSONObject("pricing");
-
+        val jsonPrice = jsonResp.getJSONObject("pricing")
         var ServerPrice = price(
                 currency = jsonPrice.getString("currency"),
                 varRate = jsonPrice.getString("vat_rate"),
